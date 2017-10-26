c5ee5c4ce401c18895f5f2574a748ee9f42a3720
Andy Bridges <Andymbridges@gmail.com>
2017-10-26 11:55:09 +0100

    Adding base files

---
+++ Ikea/itemCell.swift
@@ -0,0 +1,14 @@
+//
+//  itemCell.swift
+//  Ikea
+//
+//  Created by Andrew Bridges on 20/10/2017.
+//  Copyright © 2017 Andrew Bridges. All rights reserved.
+//
+
+import UIKit
+
+class itemCell: UICollectionViewCell {
+    
+    @IBOutlet weak var itemLabel: UILabel!
+}
